bc3bbe549ebc16fed110d05c548f1b41a995c52b
mostafahagag222 <mostafahagag222@gmail.com>
2024-09-17 19:02:39 +0300

    ay 7aga

--- BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13883dcd442d2ae51258253fe05dcd60c53a64c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90552e23e9d61ddb2e98d7c55d003742f2671a86")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
